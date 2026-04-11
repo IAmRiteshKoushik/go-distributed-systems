5c81d20911e2122620402085b2731da9dba41514
IAmRiteshKoushik <riteshkoushik39@gmail.com>
2026-04-11 20:27:00 +0530

    handle concurrent workers

--- messenger/main.go
+++ messenger/main.go
@@ -13,78 +13,96 @@ type Node struct {
 	NodeIDs   []string
 	NextMsgID int
 	mu        sync.Mutex
+	outMu     sync.Mutex
+
+	// For handling re-ordering of messages despite concurrency
+	inbox chan Message
+	wg    sync.WaitGroup
 }
 
 type Message struct {
-	Src  string         `json:"src"`
-	Dest string         `json:"dest"`
-	Body map[string]any `json:"body"`
+	Src  string                 `json:"src"`
+	Dest string                 `json:"dest"`
+	Body map[string]interface{} `json:"body"`
 }
 
-type Body struct {
-	Type      string         `json:"type"`
-	MsgID     any            `json:"msg_id,omitempty"`
-	InReplyTo any            `json:"in_reply_to,omitempty"`
-	Extra     map[string]any `json:"-"`
-}
-
-func (b Body) MarshalJSON() ([]byte, error) {
-	m := make(map[string]any)
-	m["type"] = b.Type
-	if b.InReplyTo != nil {
-		m["in_reply_to"] = b.InReplyTo
-	}
-	m["msg_id"] = b.MsgID
-	for k, v := range b.Extra {
-		m[k] = v
-	}
-	return json.Marshal(m)
-}
-
-func (n *Node) Send(dest string, body map[string]any) {
+func (n *Node) Send(dest string, body map[string]interface{}) {
 	n.mu.Lock()
-	defer n.mu.Unlock()
-
-	extra := make(map[string]any)
-	for k, v := range body {
-		if k != "type" {
-			extra[k] = v
-		}
-	}
-
-	msg := struct {
-		Src  string `json:"src"`
-		Dest string `json:"dest"`
-		Body Body   `json:"body"`
-	}{
-		Src:  n.NodeID,
-		Dest: dest,
-		Body: Body{
-			Type:  body["type"].(string),
-			MsgID: n.NextMsgID,
-			Extra: extra,
-		},
-	}
-	if inReplyTo, ok := body["in_reply_to"]; ok {
-		msg.Body.InReplyTo = inReplyTo
-	}
-
-	b, err := json.Marshal(msg)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, "marshal error:", err)
-		return
-	}
-	fmt.Println(string(b))
+	body["msg_id"] = n.NextMsgID
 	n.NextMsgID++
+	n.mu.Unlock()
+
+	msg := Message{Src: n.NodeID, Dest: dest, Body: body}
+	output, _ := json.Marshal(msg)
+
+	n.outMu.Lock()
+	fmt.Println(string(output))
+	n.outMu.Unlock()
 }
 
-func (n *Node) Reply(request Message, body map[string]any) {
-	body["in_reply_to"] = request.Body["msg_id"]
+func (n *Node) Reply(request Message, body map[string]interface{}) {
+	if msgID, ok := request.Body["msg_id"].(float64); ok {
+		body["in_reply_to"] = int(msgID)
+	}
 	n.Send(request.Src, body)
 }
 
+// Spin up workers and keep ready. All of these are listening to the a
+// buffered channel. You can spin-up as many workers as you would like.
+func (n *Node) worker() {
+	defer n.wg.Done()
+
+	for msg := range n.inbox {
+		n.HandleMessage(msg)
+	}
+}
+
+func (n *Node) HandleMessage(msg Message) {
+	body := msg.Body
+
+	msgType, ok := body["type"].(string)
+	if !ok {
+		return
+	}
+
+	switch msgType {
+	case "init":
+		// After the init message, setup all the connections
+		n.mu.Lock()
+		n.NodeID = body["node_id"].(string)
+		rawIDs := body["node_ids"].([]interface{})
+		for _, id := range rawIDs {
+			n.NodeIDs = append(n.NodeIDs, id.(string))
+		}
+		n.mu.Unlock()
+
+		// And then reply to it
+		reply := map[string]interface{}{
+			"type": "init_ok",
+		}
+		n.Reply(msg, reply)
+	case "echo":
+		reply := map[string]interface{}{
+			"type": "echo_ok",
+			"echo": msg.Body["echo"],
+		}
+		n.Reply(msg, reply)
+	default:
+	}
+}
+
 func main() {
-	node := &Node{}
+	node := &Node{
+		inbox: make(chan Message, 100),
+	}
+
+	numWorkers := 1
+	node.wg.Add(numWorkers)
+
+	for range numWorkers {
+		go node.worker()
+	}
+
 	scanner := bufio.NewScanner(os.Stdin)
 
 	for scanner.Scan() {
@@ -93,18 +111,14 @@ func main() {
 			fmt.Fprintln(os.Stderr, "Error:", err)
 			continue
 		}
-
-		msgType, _ := msg.Body["type"].(string)
-		if msgType == "init" {
-			node.NodeID = msg.Body["node_id"].(string)
-			ids := msg.Body["node_ids"].([]any)
-			for _, id := range ids {
-				node.NodeIDs = append(node.NodeIDs, id.(string))
-			}
-
-			resp := make(map[string]any)
-			resp["type"] = "init_ok"
-			node.Reply(msg, resp)
-		}
+		node.inbox <- msg
 	}
+	if err := scanner.Err(); err != nil {
+		fmt.Fprintln(os.Stderr, "Scanner error:", err)
+	}
+
+	// Close the buffered channel to avoid memory leak and then wait till the
+	// worker is done
+	close(node.inbox)
+	node.wg.Wait()
 }
